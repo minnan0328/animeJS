74388227d58eb21beff4a25dce06bbdad4464992
廖敏男 <minnan0328@gmail.com>
2019-03-21 17:54:57 +0800

    第一次上傳

---
+++ assets/js/index.js
@@ -0,0 +1,39 @@
+anime({
+    targets: '.div1',
+    translateX: 50,
+    rotate: '1turn',
+    loop: true,
+    backgroundColor: '#fff',
+    duration: 800
+})
+anime({
+    targets: '.div2',
+    translateX: 50,
+    // direction: 'reverse',
+    loop: true,
+    easing: 'easeInOutSine'
+})
+anime({
+    targets: '.div4',
+    translateX: anime.stagger(10, {
+        grid: [5, 5],
+        from: 'center',
+        axis: 'x'
+    }),
+    translateY: anime.stagger(10, {
+        grid: [5, 5],
+        from: 'center',
+        axis: 'y'
+    }),
+    rotateZ: anime.stagger([0, 90], {
+        grid: [5, 5],
+        from: 'center',
+        axis: 'x'
+    }),
+    delay: anime.stagger(200, {
+        grid: [5, 5],
+        from: 'center'
+    }),
+    easing: 'easeInOutQuad',
+    loop: true,
+});
