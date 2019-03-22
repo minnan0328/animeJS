4903f86ba10256b802ee2928dfea8bff2848b73c
廖敏男 <minnan0328@gmail.com>
2019-03-22 17:57:50 +0800

    新增 svg 動態

--- assets/js/index.js
+++ assets/js/index.js
@@ -49,12 +49,12 @@ anime({
 });
 anime({
     targets: '.div4',
-    translateX: anime.stagger(10, {
+    translateX: anime.stagger(20, {
         grid: [5, 5],
         from: 'center',
         axis: 'x'
     }),
-    translateY: anime.stagger(10, {
+    translateY: anime.stagger(20, {
         grid: [5, 5],
         from: 'center',
         axis: 'y'
@@ -71,5 +71,60 @@ anime({
     easing: 'easeInOutQuad',
     endDelay: 1000,
     direction: 'alternate',
+    loop: true
+});
+anime({
+    targets: '.SpecificPropParamsL',
+    translateX: {
+        value: 75,
+        duration: 800
+    },
+    rotate: {
+        value: 720,
+        duration: 1800,
+        easing: 'easeInOutSine'
+    },
+    scale: {
+        value: 6,
+        duration: 1600,
+        delay: 800,
+        easing: 'easeInOutQuart'
+    },
+    backgroundColor: '#FFF',
+    // easing: 'easeInOutQuad',
+    direction: 'alternate',
+    delay: 250,
+    endDelay: 1000,
+    loop: true
+});
+anime({
+    targets: '.polymorph',
+    points: [
+        {
+            value: '73.5,71.5 49.5,44.5 44.5,86.5 79.5,104.5 129.91,82.88'
+        },
+        {
+            value: '90.5,39.5 60.5,57.5 35.5,88.5 78.5,118.5 108.5,81.5'
+        },
+        {
+            value: '90.5,39.5 35.5,28.5 79.5,70.5 79.5,127.5 115.5,79.5'
+        },
+        {
+           value: [
+               '102.5,21.5 54.5,46.5 51.5,82.5 81.5,98.5 115.5,79.5',
+               '90.5,39.5 35.5,28.5 79.5,70.5 79.5,127.5 115.5,79.5',
+               '73.5,71.5 49.5,44.5 44.5,86.5 79.5,104.5 129.91,82.88',
+               '90.5,39.5 60.5,57.5 35.5,88.5 78.5,118.5 108.5,81.5',
+           ]
+        },
+        {
+            value: '102.5,21.5 54.5,46.5 51.5,82.5 81.5,98.5 115.5,79.5'
+        },
+        {
+            value: '95.5,37.5 48.5,40.5 37.5,85.5 78.5,118.5 115.5,79.5'
+        }
+    ],
+    easing: 'easeOutQuad',
+    duration: 2000,
     loop: true
 });
