887ca9f7ec2860910561b3caf1be416dc9e8ef0f
廖敏男 <minnan0328@gmail.com>
2019-03-22 14:47:36 +0800

    更正class name 及動態

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,52 @@
 anime({
-    targets: '.div1',
-    translateX: 50,
-    rotate: '1turn',
+    targets: '.cssSelector',
+    translateY: 150,
+    rotate: '45deg',
+    borderTopRightRadius: ['0%', '50%'],
+    borderBottomRightRadius: ['0%', '50%'],
+    borderBottomLeftRadius: ['0%', '50%'],
+    backgroundColor: '#000',
+    endDelay: 1000,
+    easing: 'easeInOutQuad',
+    loop: true
+})
+var elements = document.querySelectorAll('.DomNodeSelector');
+anime({
+    targets: elements,
+    translateY: 5,
     loop: true,
-    backgroundColor: '#fff',
-    duration: 800
+    delay: function (el, i, l) {
+        return i * 100;
+    },
+    endDelay: function (el, i, l) {
+        return (l - i) * 100;
+    },
+    backgroundColor: '#FFF',
+    // direction: 'alternate'
 })
 anime({
-    targets: '.div2',
-    translateX: 50,
-    // direction: 'reverse',
+    targets: '.cssProrerties',
+    translateX: 0,
+    borderRadius: ['46%','38%','45%', '50%','40%','33%','44%','50%'],
+    easing: 'easeInOutQuad',
+    loop: true
+});
+anime({
+    targets: '.cssProrerties1',
+    translateX: 0,
+    borderRadius: ['35%', '50%', '33%', '45%', '50%', '40%', '25%', '50%','47%','33%','41%','50%'],
+    easing: 'easeInOutQuad',
+    loop: true
+});
+anime({
+    targets: '.cssTransforms',
+    translateX: 0,
+    scale: 1,
+    rotate: '100turn',
+    skew: 1000,
+    easing: 'easeInOutQuad',
     loop: true,
-    easing: 'easeInOutSine'
-})
+});
 anime({
     targets: '.div4',
     translateX: anime.stagger(10, {
@@ -35,13 +69,7 @@ anime({
         from: 'center'
     }),
     easing: 'easeInOutQuad',
-    loop: true
-});
-anime({
-    targets: '.div5',
-    translateX: 50,
-    backgroundColor: '#FFF',
-    borderRadius: ['0%', '50%'],
-    easing: 'easeInOutQuad',
+    endDelay: 1000,
+    direction: 'alternate',
     loop: true
 });
